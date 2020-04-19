af7a7b03b60fb94a0f0d447eada22019c87689db
Massimo Menichinelli <info@openp2pdesign.org>
2020-04-19 13:04:01 +0200

    Replace dropdown filter with select groups filter

--- pubs.js
+++ pubs.js
@@ -1,7 +1,65 @@
-d3.select("#pubs").style("background-color", "#fff");
-
 d3.json("data/data.json")
     .then(data => {
+
+        // Add a filter for years
+        var pubsYears = ["2006", "2007", "2008", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020"];
+
+        var select = d3.select('#pubsfilter')
+            .append('select')
+            .attr('class', 'select')
+            .attr('class', "form-control")
+            .on('change', onYearsChange)
+
+        var options = select
+            .selectAll('option')
+            .data(pubsYears).enter()
+            .append('option')
+            .text(function(d) {
+                return d;
+            });
+
+        function onYearsChange() {
+            selectValue = d3.select(this).property('value');
+            console.log(selectValue);
+        };
+
+        // Add a filter for type
+        var pubsType = ["Journal article", "Conference paper", "Book", "Book chapter", "Thesis", "Report", "Conference poster", "Blog post", "Magazine article", "Software"];
+
+        var select = d3.select('#pubsfilter')
+            .append('select')
+            .attr('class', 'select')
+            .attr('class', "form-control")
+            .on('change', onTypeChange)
+
+        var options = select
+            .selectAll('option')
+            .data(pubsType).enter()
+            .append('option')
+            .text(function(d) {
+                return d;
+            });
+
+        function onTypeChange() {
+            selectValue = d3.select(this).property('value');
+            console.log(selectValue);
+        };
+
+        // Add a filter button
+        var select = d3.select('#pubsfilter')
+            .append('div')
+            .attr('class', 'input-group-append')
+            .append('label')
+            .attr('class', 'input-group-text')
+            .attr('for', 'pubsfilter')
+            .html("Filter")
+            .on('click', onFilterButton)
+
+        function onFilterButton() {
+            console.log("Filter");
+        };
+
+
         // Filter publications
         $("#link_metadesign").click(function() {
             console.log("Link on meta-design publications clicked.");
@@ -64,34 +122,36 @@ d3.json("data/data.json")
                         lang: 'en-US'
                     });
                     // Add the div of the publication
-                    pubDiv = d3.select("#pubs").append('div').attr("class","pubdiv");
+                    pubDiv = d3.select("#pubs").append('div').attr("class", "pubdiv");
                     // Add the image
                     if (item.image) {
-                        pubDiv.append("p").append("img").attr("src","data/"+item.image);
+                        pubDiv.append("p").append("img").attr("src", "data/" + item.image);
                     }
                     // Add the citation in APA style
                     pubDiv.append("p").html(output);
                     // Add the type
-                    pubDiv.append("p").html('<span class="normal-icons"><i class="fas fa-book"></i> '+item.type+'</span>');
+                    pubDiv.append("p").html('<span class="normal-icons"><i class="fas fa-book"></i> ' + item.type + '</span>');
                     // Add the keywords
-                    pubDiv.append("p").html('<span class="normal-icons"><i class="fas fa-tags"></i> '+item.keywords+'</span>');
+                    pubDiv.append("p").html('<span class="normal-icons"><i class="fas fa-tags"></i> ' + item.keywords + '</span>');
                     // Add the description
-                    pubDiv.append("p").attr("class","pub-description").html(item.description);
+                    pubDiv.append("p").attr("class", "pub-description").html(item.description);
                     // Button for downloading the publication
                     if (item.pdf) {
-                        pubDiv.append('a').attr("href","data/"+item.pdf).html('<button type="button" class="btn btn-primary">Text <i class="fas fa-file-pdf"></i></button>');
+                        pubDiv.append('a').attr("href", "data/" + item.pdf).html('<button type="button" class="btn btn-primary">Text <i class="fas fa-file-pdf"></i></button>');
                     }
                     // Button for downloading the reference
-                    pubDiv.append('a').attr("href","data/"+item.bibtex).html('<button type="button" class="btn btn-primary">Reference file <i class="fas fa-download"></i></button>');
+                    pubDiv.append('a').attr("href", "data/" + item.bibtex).html('<button type="button" class="btn btn-primary">Reference file <i class="fas fa-download"></i></button>');
                     // Button for collapsible reference
-                    idToCollapse = "iD"+item.bibtex.replace('.','');
-                    pubDiv.append('button').attr("class","btn btn-primary").attr("type","button").attr("data-toggle","collapse").attr("data-target", "#"+idToCollapse).attr("aria-expanded","false").attr("aria-controls",idToCollapse).html('Reference code <i class="fas fa-code"></i>');
+                    idToCollapse = "iD" + item.bibtex.replace('.', '');
+                    pubDiv.append('button').attr("class", "btn btn-primary").attr("type", "button").attr("data-toggle", "collapse").attr("data-target", "#" + idToCollapse).attr("aria-expanded", "false").attr("aria-controls", idToCollapse).html('Reference code <i class="fas fa-code"></i>');
                     // Collapsible reference
-                    pubDiv.append("div").attr("class","collapse-separator");
-                    pubDiv.append("div").attr("class","collapse").attr("id",idToCollapse).append("div").attr("class","card card-body").append("pre").attr("class", "bibtex").html(bibcontent);
+                    pubDiv.append("div").attr("class", "collapse-separator");
+                    pubDiv.append("div").attr("class", "collapse").attr("id", idToCollapse).append("div").attr("class", "card card-body").append("pre").attr("class", "bibtex").html(bibcontent);
                 });
 
         });
+
+
     })
     .catch(error => {
         console.error(error);
--- pubsviz.js
+++ pubsviz.js
@@ -1,1 +1,4 @@
-d3.select("#pubsviz").style("background-color", "white");
+d3.select("#pubsviz").style("background-color", "#fff");
+
+
+// http://bl.ocks.org/williaster/10ef968ccfdc71c30ef8
